f78acf76593426b5332b459b9264276acd9d3a6d
ThePhantomGamer <ThePhantomGamer@users.noreply.github.com>
2016-07-01 15:54:02 -0400

    Update rmfbanner.user.js
    Added code

--- rmfbanner.user.js
+++ rmfbanner.user.js
@@ -1,1 +1,17 @@
+// ==UserScript==
+// @name         Remove Facebook Join Banner
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  see title
+// @author       Yaseen S.
+// @match        https://www.facebook.com/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    console.log('rmfbanner: removing banner');
+    var annoyingBanner = document.getElementById('pagelet_growth_expanding_cta');
+    annoyingBanner.innerHTML = '';
+    console.log('rmfbanner: finished');
+})();
